8808da8c450bd0200a2299f6c6b3df7d14b8b9c9
André Sanguinette <andreedutech@gmail.com>
2026-04-05 18:24:36 -0300

    add api webhook for clerk data sync

---
+++ src/app/api/clerk/webhook/route.ts
@@ -0,0 +1,29 @@
+
+import { db } from "@/server/db";
+
+export const POST = async (req: Request) => {
+    const body = await req.json() as { data: unknown };
+    const { data } = body;
+
+    const emailAddress = (data as any)?.email_addresses?.[0]?.email_address;
+    const firstName = (data as any)?.first_name;
+    const lastName = (data as any)?.last_name;
+    const id = (data as any)?.id;
+    const imageURL = (data as any)?.image_url
+
+    if(emailAddress === undefined || firstName === undefined || lastName === undefined || id === undefined || imageURL === undefined) {
+        return new Response('Invalid payload', { status: 400 });
+    }
+    
+    await db.user.create({
+        data: {
+            id: id,
+            firstName: firstName,
+            lastName: lastName,
+            emailAddress: emailAddress,
+            imageUrl: imageURL
+        }
+    })
+
+    return new Response('Webhook received', { status: 200 });
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   return (
-   <Button>Criar</Button>
+   <h1 className="flex justify-center items-center font-heading">Bem-vindo à nossa aplicação</h1>
   );
 }
